c4c92a39cbd5e6d141e035412073591f12b1b694
chino_desu <33673786+kafuuchino-desu@users.noreply.github.com>
2018-12-26 22:55:21 +0800

    easier teleport

--- mcdbot.py
+++ mcdbot.py
@@ -57,7 +57,11 @@ def onServerInfo(server, info):
             for bot in botlist:
               if bot.name == args[2]:
                 if bot.owner == info.player:
+                  server.execute('gamemode spectator ' + args[2])
+                  sleep(0.1)
                   server.execute('tp ' + args[2] + ' ' + info.player)
+                  sleep(0.1)
+                  server.execute('gamemode creative ' + args[2])
                 else:
                   server.tell(info.player, '你不是这个bot的主人')
         elif (args[1] == 'gm') and (len(args) == 4):
